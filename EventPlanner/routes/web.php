b67450d445838cb693cb597fc2290d12ab25f281
Martijn339074 <339074@student.mboutrecht.nl>
2025-05-13 13:43:55 +0200

    Changed database config page for mysql

--- EventPlanner/routes/web.php
+++ EventPlanner/routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
-include __DIR__ . '/../routes/T_web.php';
-include __DIR__ . '/../routes/M_web.php';
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Include test routes
-include __DIR__ . '/T_web.php';
-
-// Define main routes
-Route::middleware(['web'])->group(function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
